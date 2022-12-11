58aa0d9012680ab9e02472ea7d44f533c04a74d9
PrantoshB <Pbiswas1007@gmail.com>
2022-12-11 13:07:10 +0530

    Add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,35 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+/* eslint-disable */
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-
-    mode: 'development',
+    mode: "none",
     entry: {
-        index: './src/index.js',
+        index: "./src/index.js",
     },
     devServer: {
-        static: {
-            directory: path.join(__dirname, 'public'),
-        },
-        compress: true,
-        port: 9000,
+        static: "./dist",
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Output Management',
+            template: "./src/index.html",
         }),
     ],
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
         clean: true,
     },
     optimization: {
-        runtimeChunk: 'single',
+        runtimeChunk: "single",
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: ["style-loader", "css-loader"],
             },
         ],
     },
 };
-
